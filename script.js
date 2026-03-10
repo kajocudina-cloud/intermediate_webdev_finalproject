ca56a95f75cd4fbda7a1df9ce1adfd023e8f924f
kajocudina-cloud <kajo.cudina@gmail.com>
2026-03-10 18:38:20 -0400

    Fixed variable redeclaration and added Number conversion

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const calculate = () => {
   let simpleInterest = (p * r * t) / 100;
   let amount = p - simpleInterest;
 
-  let result = document.getElementById("result");
+    let p = Number(document.getElementById("principal").value);
+    let r = Number(document.getElementById("rate").value);
+    let t = Number(document.getElementById("time").value);
+
 
   result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
